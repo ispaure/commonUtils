a254d7821a4bac84caac7b6da00d69fefa9533df
Marc-Andre [macOS] <marcandre.voyer@gmail.com>
2025-10-26 11:38:14 -0400

    added return type on get_marc_dropbox_root

--- marcUtils.py
+++ marcUtils.py
@@ -5,5 +5,5 @@ from commonUtils import fileUtils
 from commonUtils.osUtils import *
 
 
-def get_marc_dropbox_root():
+def get_marc_dropbox_root() -> Path:
     return Path(fileUtils.get_user_home_dir(), 'Yagi Dropbox', 'Marc-Andre Voyer')
