4045383a6888fcdf3a69535cc171b45562927a03
marca[MBA] <marcandre.voyer@gmail.com>
2025-10-26 13:38:13 -0400

    fixed resolve of path on goat-pc for dropbox

--- marcUtils.py
+++ marcUtils.py
@@ -1,9 +1,12 @@
 from pathlib import Path
-
+import os
 # Common utilities
 from commonUtils import fileUtils
 from commonUtils.osUtils import *
 
 
 def get_marc_dropbox_root() -> Path:
+    if get_os() == OS.WIN:
+        if os.path.isdir('B:\\Yagi Dropbox'):
+            return Path('B:\\Yagi Dropbox\\Marc-Andre Voyer')
     return Path(fileUtils.get_user_home_dir(), 'Yagi Dropbox', 'Marc-Andre Voyer')
