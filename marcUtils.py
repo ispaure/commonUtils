adc4fe7db32a6047069d51dca369f4b825881511
Bazzite [Lenovo] <36109884+ispaure@users.noreply.github.com>
2026-02-02 19:19:24 -0500

    cleaned up paths

--- marcUtils.py
+++ marcUtils.py
@@ -9,4 +9,4 @@ def get_marc_dropbox_root() -> Path:
     if get_os() == OS.WIN:
         if os.path.isdir('B:\\Yagi Dropbox'):
             return Path('B:\\Yagi Dropbox\\Marc-Andre Voyer')
-    return Path(fileUtils.get_user_home_dir(), 'Yagi Dropbox', 'Marc-Andre Voyer')
+    return fileUtils.get_user_home_dir() / 'Yagi Dropbox' / 'Marc-Andre Voyer'
